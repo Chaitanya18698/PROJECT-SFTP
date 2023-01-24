f2f42c6d6d8973cd482a9e886e4b5cdc0d2df8f3
maheshmahi5 <maheshbora13@gmail.com>
2023-01-24 18:57:19 +0530

    upload spinner and delete download dropdown added

--- src/app/common/shimmers/shimmers.component.ts
+++ src/app/common/shimmers/shimmers.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-shimmers',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShimmersComponent implements OnInit {
   numOfCols:any = 5;
+  @Input() type:any = 'default';
 
   constructor() { }
 
